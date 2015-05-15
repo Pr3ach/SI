4aef7da46466f6f148fa10940183ab321461cfb6
Pr3acher <Pr3acher777h@gmail.com>
2015-05-15 11:51:47 +0200

    init

--- make.bat
+++ make.bat
@@ -1,5 +1,6 @@
 @echo off
 windres -i res/resource.rc -O coff -o res/resource.res
+mkdir build
 gcc src/core.c src/init.c src/main.c src/option.c res/resource.res -lcomctl32 -lcomdlg32 -lgdi32 -mwindows -o build/Storm_Inj3ctor.exe
 del res/resource.res
 
